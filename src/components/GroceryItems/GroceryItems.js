3a5200ea5dc7f3356575bd1f0674ea1295e811e5
larry adams <ladams1776@yahoo.com>
2019-04-20 19:04:23 -0400

    Creating a quick GroceryItems list Component

---
+++ src/components/GroceryItems/GroceryItems.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+
+const GROCERY_ITEMS = [
+    {
+        name: "Bacon", price: "5.79"
+    },
+    {
+        name: "Hotdogs", price: "0.79"
+    },
+    {
+        name: "Beans", price: "0.79"
+    },
+    {
+        name: "Jelly", price: "7.79"
+    },
+    {
+        name: "Peanut Butter", price: "2.79"
+    },
+    {
+        name: "Butter", price: "0.79"
+    },
+    {
+        name: "Milk", price: "0.79"
+    },
+    {
+        name: "Sugar", price: "5.00"
+    },
+    {
+        name: "Bars", price: "2.79"
+    },
+    {
+        name: "Veggies", price: "1.79"
+    },
+];
+
+
+
+export default class GroceryItems extends Component {
+    render() {
+
+        return (
+            <div>
+                <table>
+                    <tbody>
+                    <tr>
+                        <th>Item Price</th>
+                        <th>Item Name</th>
+                    </tr>
+                    </tbody>
+                    <tbody>
+                    {GROCERY_ITEMS.map((item, index) => {
+                        return <tr id={index}>
+                            <td>{item.price}</td>
+                            <td>{item.name}</td>
+                        </tr>                        
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
