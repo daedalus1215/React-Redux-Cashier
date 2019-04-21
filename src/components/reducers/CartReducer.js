239bb91ffffff2c59d7bf4985b78dab03be00cd7
larry adams <ladams1776@yahoo.com>
2019-04-21 08:26:47 -0400

    elevated state up to CartReducer

--- src/components/reducers/CartReducer.js
+++ src/components/reducers/CartReducer.js
@@ -1,14 +1,57 @@
-const CartReducer = (state, action) => {
+const GROCERY_ITEMS = [
+    {
+        name: "Bacon", price: 5.79
+    },
+    {
+        name: "Hotdogs", price: 0.79
+    },
+    {
+        name: "Beans", price: 0.79
+    },
+    {
+        name: "Jelly", price: 7.79
+    },
+    {
+        name: "Peanut Butter", price: 2.79
+    },
+    {
+        name: "Butter", price: 0.79
+    },
+    {
+        name: "Milk", price: 0.79
+    },
+    {
+        name: "Sugar", price: 5.00
+    },
+    {
+        name: "Bars", price: 2.79
+    },
+    {
+        name: "Veggies", price: 1.79
+    },
+];
 
+
+
+
+const CartReducer = (state, action) => {
+    console.log("action: ", action);
     if (state === undefined) {
         return {
+            forSale: GROCERY_ITEMS,
             cart: []
         }
     }
 
     switch (action.type) {
         case 'ADD_TO_CART': {
-
+            console.log('add to cart');
+            const cart = [...state.cart, action.item];        
+            
+            return {
+                ...state,
+                cart: cart
+            };
         }
         case 'REMOVE_FROM_CART': {
 
