006778d32b7e7a17c400d10b179dc3f897ced18e
larry adams <ladams1776@yahoo.com>
2019-04-21 08:27:06 -0400

    Cleaning up App to use react-redux

--- src/App.js
+++ src/App.js
@@ -5,37 +5,14 @@ import './style.css';
 
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cart: []
-        }
-        this.addToCart = this.addToCart.bind(this);
-        this.deleteFromCart = this.deleteFromCart.bind(this);
-    }
-
-
-    addToCart(item) {
-        const cart = [...this.state.cart, item];
-        this.setState({cart});
-    }
-
-    deleteFromCart(index) {
-        const cart = [...this.state.cart];
-        cart.splice(index, 1);
-        this.setState({cart});
-    }
-
     render() {
-        return (
-            <div id="app-container">
-                <h1>Grocery Cart</h1>
-                <p>Here's is my react App default component</p>
-                <div id="grocery-container">
-                    <GroceryItems addToCart={this.addToCart}/>
-                    <GroceryCart items={this.state.cart} removeFromCart={this.deleteFromCart}/>
+        return <div id="app-container">
+                    <h1>Grocery Cart</h1>
+                    <div id="grocery-container">
+                        <GroceryItems/>
+                        <GroceryCart />
+                    </div>
                 </div>
-            </div>
-        );
+
     }
 }
