8a3ebc43d7c7abbb2c77121129778b8d9f67aee7
larry adams <ladams1776@yahoo.com>
2019-04-21 08:26:26 -0400

    GroceryItems is now tied into react-redux

--- src/components/GroceryItems/GroceryItems.js
+++ src/components/GroceryItems/GroceryItems.js
@@ -1,42 +1,11 @@
 import React, { Component } from 'react';
-
-
-const GROCERY_ITEMS = [
-    {
-        name: "Bacon", price: 5.79
-    },
-    {
-        name: "Hotdogs", price: 0.79
-    },
-    {
-        name: "Beans", price: 0.79
-    },
-    {
-        name: "Jelly", price: 7.79
-    },
-    {
-        name: "Peanut Butter", price: 2.79
-    },
-    {
-        name: "Butter", price: 0.79
-    },
-    {
-        name: "Milk", price: 0.79
-    },
-    {
-        name: "Sugar", price: 5.00
-    },
-    {
-        name: "Bars", price: 2.79
-    },
-    {
-        name: "Veggies", price: 1.79
-    },
-];
+import {connect} from 'react-redux';
 
 
 
-export default class GroceryItems extends Component {
+
+
+class GroceryItems extends Component {
     render() {
         return (
             <div className="grocery-items">
@@ -48,12 +17,10 @@ export default class GroceryItems extends Component {
                     </tr>
                     </tbody>
                     <tbody>
-                    {GROCERY_ITEMS.map((item, index) => {
+                    {this.props.items.map((item, index) => {
                         return <tr key={index}>
                             <td>
-                                <button item={item} onClick={() => {
-                                    this.props.addToCart(item)
-                                }}>
+                                <button item={item} onClick={ () => {this.props.addToCart(item)} }>
                                     Add to cart
                                 </button>
                             </td>
@@ -67,3 +34,23 @@ export default class GroceryItems extends Component {
         );
     }
 }
+
+
+function mapStateToProps(state) {
+    return {
+        items: state.forSale
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        addToCart: (item) => {
+            dispatch({
+                type: 'ADD_TO_CART',
+                item: item
+            });
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GroceryItems)
