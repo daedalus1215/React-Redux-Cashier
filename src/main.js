48e012dbe7d5eccbc23a80baae8c2d3d24002c91
larry adams <ladams1776@yahoo.com>
2019-04-21 08:08:26 -0400

    Incorporate and setup React-Redux into the project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {createStore} from 'redux';
 import App from './App.js';
 
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import CartReducer from './components/reducers/CartReducer';
+
+
+const store = createStore(CartReducer);
+
+ReactDOM.render(
+    <Provider store={store}> 
+            <App />
+    </Provider>, 
+    document.getElementById('root')
+    );
